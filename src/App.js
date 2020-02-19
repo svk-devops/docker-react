421bbc90ad1a721a6e90ac1b079a7773a5d19b7c
Serhiy Kozlov <sergey.kozlov@hotmail.com>
2020-02-19 16:34:10 +0200

    az web app

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload - Trvis CI.
+          Azure Web App - Docker - Trvis CI.
         </p>
         <a
           className="App-link"
